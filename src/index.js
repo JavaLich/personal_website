9d74345113dac8a835f44bd39023aeed2c8ae70d
Akash Melachuri <amelachuri@gmail.com>
2021-05-30 14:27:57 -0500

    Handling resizing

--- src/index.js
+++ src/index.js
@@ -5,37 +5,40 @@ var width = window.innerWidth;
 var height = window.innerHeight;
 var scale = 8;
 
-const scene = new THREE.Scene();
-const camera = new THREE.OrthographicCamera(width / - 2, width / 2, height / 2, height / - 2, 1, 1000);
+
+var scene = new THREE.Scene();
+var camera = new THREE.OrthographicCamera(width / - 2, width / 2, height / 2, height / - 2, 1, 1000);
 
 const canvas = document.querySelector('#c');
 const renderer = new THREE.WebGLRenderer({canvas});
 renderer.setSize(width, height);
 
-const geometry = new THREE.PlaneGeometry(width / 2, height / 2, 1, 1);
-// const material = new THREE.MeshBasicMaterial({color: 0xffffff});
- const material = new THREE.ShaderMaterial( {
-     vertexShader: document.getElementById( 'vertex' ).textContent,
- 	fragmentShader: document.getElementById( 'fragment' ).textContent
- } )
-
-console.log(material.vertexShader)
-
-// for (let y = 0; y < height / scale; y++) {
-//     for (let x = 0; x < width / scale; x++) {
-//         const mesh = new THREE.Mesh( geometry, material );
-//         
-//         mesh.position.x = x * scale - width / 2 + scale / 2;
-//         mesh.position.y = y * scale - height / 2 + scale / 2;
-//         
-//         scene.add( mesh );
-//     }
-// }
-
-const mesh = new THREE.Mesh( geometry, material );
+var geometry = new THREE.PlaneGeometry(width, height, 1, 1);
+var material = new THREE.ShaderMaterial( {
+    vertexShader: document.getElementById( 'vertex' ).textContent,
+	fragmentShader: document.getElementById( 'fragment' ).textContent
+} );
+var mesh = new THREE.Mesh( geometry, material );
 scene.add( mesh );
 
 camera.position.z = 1;
+
+function handleResize() {
+    width = window.innerWidth;
+    height = window.innerHeight;
+    camera = new THREE.OrthographicCamera(width / - 2, width / 2, height / 2, height / - 2, 1, 1000);
+    camera.position.z = 1;
+    renderer.setSize(width, height);
+    geometry = new THREE.PlaneGeometry(width, height, 1, 1);
+    material = new THREE.ShaderMaterial( {
+        vertexShader: document.getElementById( 'vertex' ).textContent,
+    	fragmentShader: document.getElementById( 'fragment' ).textContent
+    } );
+    mesh = new THREE.Mesh( geometry, material );
+    scene = new THREE.Scene();
+    scene.add( mesh );
+}
+window.addEventListener('resize', handleResize);
 
 function animate() {
     requestAnimationFrame(animate);
