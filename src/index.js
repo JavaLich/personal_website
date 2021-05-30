edf1bdc46a3301d73127363ec7d10bbea1a15f65
Akash Melachuri <amelachuri@gmail.com>
2021-05-30 11:53:39 -0500

    Added shaders

--- src/index.js
+++ src/index.js
@@ -1,32 +1,38 @@
 import * as THREE from 'three';
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
 import './style.css'
 
+var width = window.innerWidth;
+var height = window.innerHeight;
+var scale = 8;
+
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.OrthographicCamera(width / - 2, width / 2, height / 2, height / - 2, 1, 1000);
 
 const canvas = document.querySelector('#c');
 const renderer = new THREE.WebGLRenderer({canvas});
-renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setSize(width, height);
 
-const controls = new OrbitControls(camera, canvas);
-controls.target.set(0, 0, 0);
-controls.update();
+const geometry = new THREE.PlaneGeometry(width / 2, height / 2, 1, 1);
+// const material = new THREE.MeshBasicMaterial({color: 0xffffff});
+ const material = new THREE.ShaderMaterial( {
+     vertexShader: document.getElementById( 'vertex' ).textContent,
+ 	fragmentShader: document.getElementById( 'fragment' ).textContent
+ } )
 
-var geometry = new THREE.BufferGeometry();
-const vertices = new Float32Array( [
-  -1.0, -1.0,  0.0,
-	 1.0, -1.0,  0.0,
-	 1.0,  1.0,  0.0,
+console.log(material.vertexShader)
 
-	 1.0,  1.0,  0.0,
-	-1.0,  1.0,  0.0,
-	-1.0, -1.0,  0.0	] );
+// for (let y = 0; y < height / scale; y++) {
+//     for (let x = 0; x < width / scale; x++) {
+//         const mesh = new THREE.Mesh( geometry, material );
+//         
+//         mesh.position.x = x * scale - width / 2 + scale / 2;
+//         mesh.position.y = y * scale - height / 2 + scale / 2;
+//         
+//         scene.add( mesh );
+//     }
+// }
 
-geometry.setAttribute( 'position', new THREE.BufferAttribute( vertices, 3 ) );
-const material = new THREE.MeshBasicMaterial( { color: 0x000000 } );
 const mesh = new THREE.Mesh( geometry, material );
-
 scene.add( mesh );
 
 camera.position.z = 1;
--- webpack.config.js
+++ webpack.config.js
@@ -20,8 +20,6 @@ module.exports = {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
-        library: "threejs",
-        libraryTarget: "umd" 
     },
     module: {
         rules: [
