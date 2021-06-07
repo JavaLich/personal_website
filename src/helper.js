04d35c56aff1cadf3d78550ba075b95cc6d24962
Akash Melachuri <amelachuri@gmail.com>
2021-06-07 10:32:36 -0500

    Made framebuffer feedback handle its own shaders

--- src/helper.js
+++ src/helper.js
@@ -5,13 +5,35 @@ export class FramebufferFeedback {
         this.width = width;
         this.height = height;
 
-        this.targetA = new THREE.WebGLRenderTarget(width, height);
-        this.targetB = new THREE.WebGLRenderTarget(width, height);
+        this.target = new THREE.WebGLRenderTarget(width, height);
+        this.temp = new THREE.WebGLRenderTarget(width, height);
+
+        this.scene = new THREE.Scene();
+        this.geometry = new THREE.PlaneGeometry(width, height);
+        this.material = new THREE.ShaderMaterial({
+            vertexShader: document.getElementById('vertex').textContent,
+    	    fragmentShader: document.getElementById('fragment').textContent,
+            uniforms: {
+                click: {value: false},
+                density: {value: this.temp.texture }
+            }
+        });
+        this.mesh = new THREE.Mesh(this.geometry, this.material);
+        this.scene.add(this.mesh);
+    }
+
+    update(renderer, camera) {
+        renderer.setRenderTarget(this.target)
+        renderer.render(this.scene, camera);
+
+        this.swap();
+
+        this.material.uniforms.density.value = this.temp.texture;
     }
 
     swap() {
-        var t = this.targetA;
-        this.targetA = this.targetB;
-        this.targetB = t;
+        var t = this.target;
+        this.target = this.temp;
+        this.temp = t;
     }
 }
--- src/index.js
+++ src/index.js
@@ -2,15 +2,13 @@ import * as THREE from 'three';
 import './style.css';
 import * as Helper from './helper.js'
 
-var click = false;
-
 function mousePress() {
-    click = true;
+    density.material.uniforms.click.value = true;
 }
 window.addEventListener('mousedown', mousePress);
 
 function mouseRelease() {
-    click = false;
+    density.material.uniforms.click.value = false;
 }
 window.addEventListener('mouseup', mouseRelease);
 function handleResize() {
@@ -23,28 +21,14 @@ window.addEventListener('resize', handleResize);
 
 function init() {
     scene = new THREE.Scene();
-    bufferScene = new THREE.Scene();
     camera = new THREE.OrthographicCamera(width / - 2, width / 2, height / 2, height / - 2, 1, 1000);
     camera.position.z = 1;
 
     renderer.setSize(width, height);
 
-    geometry = new THREE.PlaneGeometry(width, height);
-    material = new THREE.ShaderMaterial({
-        vertexShader: document.getElementById('vertex').textContent,
-    	fragmentShader: document.getElementById('fragment').textContent,
-        uniforms: {
-            click: {value: click},
-            density: {value: density.targetA.texture }
-        }
-    });
-    mesh = new THREE.Mesh(geometry, material);
-
-    bufferScene.add(mesh);
-
     var box = new THREE.PlaneGeometry(width, height);
     const box_material = new THREE.MeshBasicMaterial( {
-        map: density.targetB.texture,
+        map: density.temp.texture,
     } );
 
     box_mesh = new THREE.Mesh(box, box_material);
@@ -75,15 +59,9 @@ init();
 function animate() {
     requestAnimationFrame(animate);
 
-    mesh.material.uniforms.click.value = click;
+    density.update(renderer, camera);
 
-    renderer.setRenderTarget(density.targetB)
-    renderer.render(bufferScene, camera);
-
-    density.swap();
-
-    box_mesh.material.map = density.targetB.texture;
-    material.uniforms.density.value = density.targetA.texture;
+    box_mesh.material.map = density.target.texture;
 
     renderer.setRenderTarget(null)
     renderer.render(scene, camera);
